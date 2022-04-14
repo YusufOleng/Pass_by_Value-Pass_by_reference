60ea192fbee6bb7946d152609d9491c4502d98cd
ucup Markucup <81012410+YusufOleng@users.noreply.github.com>
2022-04-14 10:28:05 +0700

    Add files via upload

---
+++ Pass by Reference.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Program{
+private:
+	static void Kuadrat(int &x);
+
+public:
+	static void Main();
+
+};
+
+void Program::Kuadrat(int &x){
+	x = x * x;
+	cout << x <<endl;
+}
+
+void Program::Main()
+{
+	int a = 3;
+	Kuadrat(a);
+	cout << a <<endl;
+}
+
+int main(){
+	Program testing;
+
+	testing.Main();
+
+	cin.get();
+}
